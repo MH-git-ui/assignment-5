f1536a2ad2c74dc8c3c289e22199308afc3ee7ba
MH-git-ui <mh4846111@gmail.com>
2026-09-12 16:31:44 +0600

    Adding technology and data in src

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
-import type { Technology } from './types/Technology'
+import type { Technology } from './type/Technology'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import TechnologyGrid from './components/Technology_Grid'
